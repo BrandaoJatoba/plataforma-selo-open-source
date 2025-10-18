c2db591b74e4de9f1ed3906a94a24bcf67d01d1e
João Felipe Brandão Jatobá <brandaojatoba@gmail.com>
2025-10-18 16:45:49 -0300

    test: cria teste de integração para o AuthForm

--- selo-fiea-frontend/vite.config.ts
+++ selo-fiea-frontend/vite.config.ts
@@ -1,5 +1,5 @@
 // vite.config.ts
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' 
 
@@ -8,4 +8,10 @@ export default defineConfig({
     react(),
     tailwindcss(), 
   ],
+  test: {
+    globals: true, // Permite usar expect, describe, test sem importação
+    environment: 'jsdom', // Simula o ambiente DOM do navegador
+    setupFiles: './src/setupTests.ts', // Caminho para o seu arquivo de setup
+    css: true, 
+  }
 })
